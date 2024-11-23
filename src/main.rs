f5b11b68b415144cac0fa5be2bc0459e789032cd
moulin-louis <louis.moulin@outlook.fr>
2024-11-23 17:55:40 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 use leptos::*;
+use thaw::Button;
+
+#[server(TestServerFn, "test_server_fn", "GetJson")]
+pub async fn test_server_fn() -> Result<(), ServerFnError> {
+    println!("hello from server");
+    Ok(())
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+    logging::log!("app mounted");
+    view! {
+        <Button on:click=move |_| {
+            spawn_local(async {
+                test_server_fn().await.unwrap();
+            });
+        }>
+            "Click here to run server fn:" {count}
+        </Button>
+    }
+}
 
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| {
-        view! { <p>"Hello worlds"</p> }
+        view! { <App /> }
     });
     println!("Hello, world!");
 }
